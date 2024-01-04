d2fa1806e5aeb02aecea325eb77fb3cb3cd1efec
calebbabin <object.caleb@gmail.com>
2024-01-03 23:35:10 -0500

    improve components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,11 @@
 const buttonBaseClass = 'px-4 py-1 rounded transition-all duration-100 font-bold'
 export function Button(props) {
 	return <button
-		onClick={props.onClick}
-		className={buttonBaseClass + " border border-sky-500 hover:bg-sky-500 " + props.className}
+		disabled={props.disabled}
+		onClick={() => {
+			if (!props.disabled) props.onClick();
+		}}
+		className={buttonBaseClass + " border border-sky-500 hover:bg-sky-500 disabled:opacity-50 disabled:cursor-not-allowed " + props.className}
 	>
 		{props.children}
 	</button>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,6 +2,6 @@
 export default function TextInput(props) {
 	return <input
 		{...props}
-		className={props.className + " p-2 border border-sky-500 rounded-md"}
+		className={props.className + " p-2 border border-sky-500 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"}
 	/>
 }
