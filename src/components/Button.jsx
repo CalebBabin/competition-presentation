97b8d4a5c7240ccf549569b900219d57ef33ff02
calebbabin <object.caleb@gmail.com>
2024-01-04 11:58:54 -0500

    update styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 
-const buttonBaseClass = 'px-4 py-1 rounded transition-all duration-100 font-bold'
 export function Button(props) {
+	const style = {};
+
+	if (props.icon) style.paddingLeft = '2.75rem';
 	return <button
 		disabled={props.disabled}
 		onClick={() => {
-			if (!props.disabled) props.onClick();
+			if (!props.disabled && props.onClick) props.onClick();
 		}}
-		className={buttonBaseClass + " border border-sky-500 hover:bg-sky-500 disabled:opacity-50 disabled:cursor-not-allowed " + props.className}
+		className={"relative px-4 py-2 h-12 rounded-[1.5rem] overflow-hidden transition-all duration-100 disabled:opacity-50 disabled:cursor-not-allowed border text-white font-bold border-sky-500 hover:bg-sky-500 " + props.className}
+		style={style}
 	>
+		{props.to ? <Link to={props.to} className="absolute inset-0" /> : null}
+		{props.icon ? <span className="material-symbols-outlined text-[2.5rem] w-10 h-10 absolute left-[.25rem] top-[0.25rem]">{props.icon}</span> : null}
 		{props.children}
 	</button>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-
+import { Button } from "../components/Button"
+import PageLayoutWrapper from "../components/PageLayoutWrapper"
 
 function Home() {
 	return (
-		<div>
-			Home!
-		</div>
+		<PageLayoutWrapper>
+			<div className="flex flex-col gap-4 justify-stretch">
+				<Button to='/setup' icon="add">
+					Setup New Competition
+				</Button>
+				<Button to='/competition' icon="refresh">
+					Resume saved
+				</Button>
+			</div>
+
+
+		</PageLayoutWrapper>
 	)
 }
 
