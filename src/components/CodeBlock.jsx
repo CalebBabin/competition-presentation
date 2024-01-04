040a7ef103ac3500980db990cab61a7210306825
calebbabin <object.caleb@gmail.com>
2024-01-03 23:34:26 -0500

    pretty code block :)

---
+++ src/components/CodeBlock.jsx
@@ -0,0 +1,16 @@
+import { useMemo } from "react"
+
+export function CodeBlock(props) {
+	const children = useMemo(() => {
+		if (!props.text) return;
+		const array = [];
+		const textArray = props.text.split('\n');
+		for (let index = 0; index < textArray.length; index++) {
+			array.push(<div className="hover:bg-slate-700/80 -mx-4 px-4" key={index}>{textArray[index]}</div>);
+		}
+		return array;
+	}, props.text)
+	return <pre className={props.className + " font-mono whitespace-pre bg-slate-800 text-white rounded-lg p-4 border-2 border-white/20 max-w-full overflow-auto"}>
+		{children}
+	</pre>
+}
