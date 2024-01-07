3c827455a71c414a493257b536c57d50b5c9655e
calebbabin <object.caleb@gmail.com>
2024-01-07 12:24:55 -0500

    add expand/collapse functionality to overviews

--- src/components/ItemPreview.jsx
+++ src/components/ItemPreview.jsx
@@ -1,5 +1,5 @@
 
-export function ItemPreview({ item }) {
+export function ItemPreview({ item, inactive = false }) {
 	let embed;
 	//<iframe src="https://drive.google.com/file/d/1e4qTkRBJhM7QS7W4IYtyjNcFl2EHZEUW/preview" width="640" height="480" allow="autoplay"></iframe>
 	//https://drive.google.com/open?id=1e4qTkRBJhM7QS7W4IYtyjNcFl2EHZEUW
@@ -7,20 +7,23 @@ export function ItemPreview({ item }) {
 		const url = new URL(item.data.content);
 		const file_id = url.searchParams.get('id');
 		embed = <iframe
-			src={`https://drive.google.com/file/d/${file_id}/preview`}
+			src={inactive ? '' : `https://drive.google.com/file/d/${file_id}/preview`}
 			className="aspect-video w-full h-auto"
 			width={1920}
 			height={1080}
-			allow="autoplay"
 			crossOrigin="anonymous"
 		/>
 	} else {
 		embed = <img src={item.data.content} crossOrigin="anonymous" className="w-full h-auto aspect-video object-contain" />
 	}
 
-	return <div className="w-full bg-slate-700 rounded">
-		{item.data.title && <h1 className="relative z-10 pointer-events-none text-4xl -mb-8 h-16">{item.data.title}</h1>}
-		{embed}
-		{item.data.author && <h3 className="text-xl">{item.data.author}</h3>}
+	return <div className="w-full bg-slate-700 rounded flex gap-4 items-center">
+		<div className="w-4/12">
+			{embed}
+		</div>
+		<div>
+			{item.data.title && <h1 className="relative z-10 pointer-events-none text-4xl -mb-8 h-16">{item.data.title}</h1>}
+			{item.data.author && <h3 className="text-xl">{item.data.author}</h3>}
+		</div>
 	</div>
 }
--- src/pages/Competition.jsx
+++ src/pages/Competition.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { CodeBlock } from "../components/CodeBlock";
 import { ItemPreview } from "../components/ItemPreview";
 import { useBackground } from "../util/setPageBackground"
@@ -7,12 +8,46 @@ function CategoryOverview({ category }) {
 	const count = useCategoryCount(category);
 	const items = useCategory(category);
 
-	return <div className="bg-slate-600 hover:bg-slate-500 p-2 rounded cursor-default max-w-full w-[700px]">
-		<h3>{category} - {count}</h3>
-		<div className="w-full flex flex-col gap-4">
-			{items.map(item => <ItemPreview key={item.key} item={item} />)}
+	const [expand, setExpand] = useState(false);
+
+	const ref = useRef();
+	useEffect(() => {
+		if (!ref.current) return;
+
+		let targetHeight = 0;
+		const resizeListener = () => {
+			targetHeight = ref.current.children[0].offsetHeight;
+
+			if (expand && ref.current.style.height !== targetHeight + 'px') {
+				ref.current.style.height = targetHeight + 'px';
+			} else if (!expand && ref.current.style.height !== '0px') {
+				ref.current.style.height = '0px';
+			}
+		}
+		resizeListener();
+
+		window.addEventListener('resize', resizeListener);
+		return () => {
+			window.removeEventListener('resize', resizeListener);
+		}
+	}, [ref.current, expand]);
+
+	return <div className="bg-slate-600 p-2 rounded cursor-default max-w-full w-[700px]">
+		<h3 className="text-lg mb-4">
+			{category} - {count}
+			<span
+				onClick={() => setExpand(!expand)}
+				className={(expand ? 'rotate-180' : 'rotate-0') + " transition-transform duration-200 material-symbols-outlined inline-block align-bottom ml-1 px-1 rounded hover:bg-slate-500"}
+			>
+				expand_more
+			</span>
+		</h3>
+		<div ref={ref} className="overflow-hidden transition-all duration-500">
+			<div className="w-full flex flex-col gap-4">
+				{items.map(item => <ItemPreview key={item.key} item={item} inactive={!expand} />)}
+			</div>
 		</div>
-	</div>
+	</div >
 }
 
 function Competition() {
