0f6b1df44c609eba74571b619dce1d56c2b13670
calebbabin <object.caleb@gmail.com>
2024-01-09 16:59:58 -0500

    add a 404 template

---
+++ src/components/error/NotFound.jsx
@@ -0,0 +1,7 @@
+
+export default function NotFound() {
+	return <div className="text-center">
+		<span className="text-2xl md:text-4xl lg:text-8xl xl:text-9xl">Error 404 &nbsp;</span>
+		<span className="text-xl md:text-2xl lg:text-4xl xl:text-6xl">Page not found</span>
+	</div>
+}
