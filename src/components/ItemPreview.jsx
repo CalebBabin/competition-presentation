ccce7f9385cb39a76aab32ef6ce83d4f17523417
calebbabin <object.caleb@gmail.com>
2024-01-11 22:18:39 -0600

    adjust layout

--- src/components/ItemPreview.jsx
+++ src/components/ItemPreview.jsx
@@ -20,14 +20,22 @@ export function ItemPreview({ item, inactive = false }) {
 
 	return <div
 		className="w-full bg-slate-700 rounded flex gap-4 items-center"
-		// onMouseEnter={() => { preloadPopup(item) }}
+	// onMouseEnter={() => { preloadPopup(item) }}
 	>
 		<div className="w-4/12">
 			{embed}
 		</div>
-		<div>
-			{item.data.title && <h1 className="relative z-10 pointer-events-none text-4xl -mb-8 h-16">{item.data.title}</h1>}
-			{item.data.author && <h3 className="text-xl">{item.data.author}</h3>}
+		<div className="w-8/12">
+			{item.data.title && <h1
+				title={item.data.title}
+				className="leading-none whitespace-pre text-ellipsis overflow-hidden w-full z-10 text-4xl">
+				{item.data.title}
+			</h1>}
+			{item.data.author && <h3
+				title={item.data.author}
+				className="whitespace-pre text-ellipsis overflow-hidden w-full text-xl">
+				{item.data.author}
+			</h3>}
 
 			<span
 				onClick={() => { triggerPopup(item) }}
@@ -39,7 +47,7 @@ export function ItemPreview({ item, inactive = false }) {
 	</div>
 }
 
-export function ImageEmbed ({item}) {
+export function ImageEmbed({ item }) {
 	let embed;
 	if (item.data.content.startsWith('https://drive.google.com/open')) {
 		const url = new URL(item.data.content);
