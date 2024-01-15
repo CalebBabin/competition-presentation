37e10ede012f956f79c64db4cec1c8c730252a15
calebbabin <object.caleb@gmail.com>
2024-01-15 17:14:13 -0600

    update icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout(props) {
 		<head>
 			<meta charSet="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="icon" href="/favicon.png" type="image/png" />
 			<title>Competition Decider!</title>
 		</head>
 		<body className={'dark ' + inter.className}>
